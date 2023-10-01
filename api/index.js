0d97336c9eb54a734fb15dcb1340fe6f290eba66
Emmanuel Amoo <amooemma10@gmail.com>
2023-10-01 21:50:16 +0100

    axios mapami

--- api/index.js
+++ api/index.js
@@ -4,12 +4,22 @@ import postsRoute from './routes/posts.js'
 import commentsRoute from './routes/comments.js'
 import authRoute from './routes/auth.js'
 import likesRoute from './routes/likes.js'
+import cookieParser from "cookie-parser";
+import cors from 'cors'
 
 
 const app = express();
 
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Credentials", true)
+    next()
+})
 app.use(express.json())
-
+app.use(cors({
+    origin: "http://localhost:3000",
+    
+}))
+app.use(cookieParser())
 app.use('/api/user', userRoute)
 app.use('/api/posts', postsRoute)
 app.use('/api/comments', commentsRoute)
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,7 +1,9 @@
 import express  from "express";
+import { getPosts, addPosts } from "../controllers/posts.js";
 
 const router = express.Router();
 
-
+router.get("/", getPosts)
+router.post("/", addPosts)
 
 export default router
